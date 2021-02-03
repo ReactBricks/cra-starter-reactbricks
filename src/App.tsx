989521bcd9ffe55ee0444f45d7df1e139a027533
Giorgio Tognoli <giorgio@f2.net>
2021-02-03 09:17:22 +0100

    removed import not used in page App

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,7 @@ import Editor from './admin/Editor'
 import Login from './admin/Login'
 import Playground from './admin/Playground'
 import './App.css'
-import ErrorNoHomePage from './components/ErrorNoHomePage'
 import ErrorNoKeys from './components/ErrorNoKeys'
-import Footer from './components/Footer'
-import Header from './components/Header'
 import Layout from './components/Layout'
 import config from './react-bricks/config'
 import Viewer from './Viewer'
@@ -18,24 +15,22 @@ import ViewerFetch from './ViewerFetch'
 const App: React.FC<RouteComponentProps> = () => {
   return (
     <Layout>
-      <div className="App">
-        {!config.appId || !config.apiKey ? (
-          <ErrorNoKeys />
-        ) : (
-          <ReactBricks {...config}>
-            <Router>
-              <Login path="/admin" />
+      {!config.appId || !config.apiKey ? (
+        <ErrorNoKeys />
+      ) : (
+        <ReactBricks {...config}>
+          <Router>
+            <Login path="/admin" />
 
-              <Editor path="/admin/editor" />
-              <Playground path="/admin/playground" />
-              <AppSettings path="/admin/app-settings" />
+            <Editor path="/admin/editor" />
+            <Playground path="/admin/playground" />
+            <AppSettings path="/admin/app-settings" />
 
-              <Viewer path="/" />
-              <ViewerFetch path="/viewer-fetch" />
-            </Router>
-          </ReactBricks>
-        )}
-      </div>
+            <Viewer path="/" />
+            <ViewerFetch path="/viewer-fetch" />
+          </Router>
+        </ReactBricks>
+      )}
     </Layout>
   )
 }
