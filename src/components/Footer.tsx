04b7840e1e198238a4fbcdd6ecd595d5007cfc39
Cecilia Panicali <cecilia@f2.net>
2021-02-09 17:34:54 +0100

    Rinominate classi.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,14 @@ import styles from './Footer.module.css'
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerWrapper}>
-        <div className={styles.imageWrapper}>
-          <img src="/react-bricks-icon.svg" alt="React Bricks" className={styles.image} />
-          <div className={styles.footerWrite}>Create React App starter</div>
+      <div className={styles.container}>
+        <div className={styles.logoWrapper}>
+          <img src="/react-bricks-icon.svg" alt="React Bricks" className={styles.logo} />
+          <div className={styles.starterName}>Create React App starter</div>
         </div>
-        <div className={styles.footerDate}>
+        <div className={styles.copy}>
           © {new Date().getFullYear()}{' '}
-          <a href="https://reactbricks.com" className={styles.footerLink}>
+          <a href="https://reactbricks.com" className={styles.copyLink}>
             React Bricks
           </a>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,20 +5,20 @@ import styles from './Header.module.css'
 
 const Header: React.FC = () => (
   <header className={styles.header}>
-    <div className={styles.headerWrapper}>
-      <div className={styles.headerSubWrapper}>
-        <div className={styles.headerView}>
-          <img src="/react-bricks-logo.svg" className={styles.image} alt="React Bricks" />
-          <div className={styles.groupLink}>
-            <Link to="/" className={styles.coupleLink}>
+    <div className={styles.container}>
+      <div className={styles.headerContent}>
+        <div className={styles.logoMenuWrapper}>
+          <img src="/react-bricks-logo.svg" className={styles.logo} alt="React Bricks" />
+          <div className={styles.headerMenu}>
+            <Link to="/" className={styles.menuLink}>
               Home
             </Link>
-            {/* <Link to="/about-us" className={styles.coupleLink}>
+            {/* <Link to="/about-us" className={styles.menuLink}>
               About us
             </Link> */}
           </div>
         </div>
-        <Link to="/admin" className={styles.singleLink}>
+        <Link to="/admin" className={styles.callToAction}>
           Edit content
         </Link>
       </div>
